fec93ac80de5cca7a47abeb8248a6495e7dcb498
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2022-04-09 10:36:06 +0700

    feat: add exception handling in form

--- app/src/main/java/com/example/formlearn/MainActivity.java
+++ app/src/main/java/com/example/formlearn/MainActivity.java
@@ -1,58 +1,80 @@
 package com.example.formlearn;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     RecyclerView recyclerView;
     RecyclerView.LayoutManager recylerViewLayoutManager;
-    MyAdapter myAdapter;
-    EditText etNama, etNrp, etIpk;
-    RadioButton rbCheck;
-    RadioGroup rbJk;
-    ArrayList<Mahasiswa> mhs;
+    MahasiswaAdapter mhsAdapter;
+    ArrayList<Mahasiswa> mhs = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etNama = findViewById(R.id.etNama);
-        etNrp = findViewById(R.id.etNrp);
-        etIpk = findViewById(R.id.etIpk);
-        rbJk = findViewById(R.id.rbJk);
-        mhs = new ArrayList<>();
+        Button saveButton = findViewById(R.id.save_button);
+        saveButton.setOnClickListener(this);
 
-        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView = findViewById(R.id.mahasiswa_list);
         recyclerView.setHasFixedSize(true);
         recylerViewLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(recylerViewLayoutManager);
-        myAdapter = new MyAdapter(this,mhs);
-        recyclerView.setAdapter(myAdapter);
-
+        mhsAdapter = new MahasiswaAdapter(this,mhs);
+        recyclerView.setAdapter(mhsAdapter);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
     }
 
-    public void onSubmit(View v){
-        String nama = etNama.getText()+"";
-        String nrp = etNrp.getText()+"";
-        String ipk = etIpk.getText()+"";
-        rbCheck = findViewById(rbJk.getCheckedRadioButtonId());
-        String jk = rbCheck.getText()+"";
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.save_button) {
 
-        Mahasiswa m = new Mahasiswa(nama,nrp,Double.parseDouble(ipk),jk);
+            EditText etNama = findViewById(R.id.etNama);
+            EditText etNrp = findViewById(R.id.etNrp);
+            EditText etIpk = findViewById(R.id.etIpk);
+            RadioButton rbLk = findViewById(R.id.rbLk);
+            RadioButton rbPr = findViewById(R.id.rbPr);
 
-        myAdapter.add(mhs.size(), m);
+            String nama = etNama.getText()+"";
+            String nrp = etNrp.getText()+"";
+            String ipk = etIpk.getText()+"";
+            boolean isLaki = rbLk.isChecked();
+            boolean isPerempuan = rbPr.isChecked();
 
-        Toast.makeText(this,"Berhasil menambahkan data",Toast.LENGTH_LONG).show();
+            try{
+                if(nama.isEmpty()){
+                    throw new IllegalArgumentException("Anda belum mengisi nama");
+                }
+                if(nrp.isEmpty()){
+                    throw new IllegalArgumentException("Anda belum mengisi NRP");
+                }
+                if(ipk.isEmpty()){
+                    throw new IllegalArgumentException("Anda belum mengisi IPK");
+                }
+                double ipk_double = Double.parseDouble(ipk);
+                if(ipk_double<0.0 || ipk_double>4.0){
+                    throw new NumberFormatException("Ipk harus lebih dari 0 dan kurang dari 4");
+                }
+                if(!isLaki && !isPerempuan){
+                    throw new IllegalArgumentException("Anda belum mengisi jenis kelamin");
+                }
+                Mahasiswa m = new Mahasiswa(nama,nrp,ipk_double,isLaki);
+                mhsAdapter.add(mhs.size(), m);
+                Toast.makeText(this,"Berhasil menambahkan data",Toast.LENGTH_LONG).show();
+            } catch (IllegalArgumentException e){
+                Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        }
     }
-
 }
